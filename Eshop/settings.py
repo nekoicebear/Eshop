f4fefc216e53d8b0d13983955a80cba80cb3226d
nekoicebear <nekoicebear@hs.ac.kr>
2022-06-20 08:45:30 +0900

    수정

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@2*-8r&5jtn@u)w5wwpc423u84k1yce$qr$u(ybk4)918d0l0u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'http://nekoicebear.pythonanywhere.com/', 'http://nekoicebear.pythonanywhere.com']
+ALLOWED_HOSTS = ['nekoicebear.pythonanywhere.com']
 
 
 # Application definition
