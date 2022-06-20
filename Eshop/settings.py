c8d7271f35a4893cf076c5a01c021f30035dfa5d
nekoicebear <nekoicebear@hs.ac.kr>
2022-06-20 11:11:38 +0900

    수정

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@2*-8r&5jtn@u)w5wwpc423u84k1yce$qr$u(ybk4)918d0l0u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['nekoicebear.pythonanywhere.com']
+ALLOWED_HOSTS = ['nekoicebear.pythonanywhere.com','*']
 
 
 # Application definition
